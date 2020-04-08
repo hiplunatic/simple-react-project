a607b6035b6930504353fde20cbae7f9b5debe6b
David Jensen <djensen81@gmail.com>
2020-04-08 11:27:37 -0400

    Updates

---
+++ precache-manifest.685163ea5a22dcff96d2c97fe77033e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20155bd69e39126ca79257fdac3f78a",
+    "url": "/simple-react-project/index.html"
+  },
+  {
+    "revision": "4b1b6530d4f68eef0d1f",
+    "url": "/simple-react-project/static/css/main.672538a0.chunk.css"
+  },
+  {
+    "revision": "7dacad236343b803e4db",
+    "url": "/simple-react-project/static/js/2.43aa1611.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-project/static/js/2.43aa1611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1b6530d4f68eef0d1f",
+    "url": "/simple-react-project/static/js/main.0927daf5.chunk.js"
+  },
+  {
+    "revision": "475390f31a23a882f568",
+    "url": "/simple-react-project/static/js/runtime-main.ad396956.js"
+  }
+]);
