6b455b9dad2ad597b48a07d636e75ae7dbd05593
David Jensen <djensen81@gmail.com>
2020-04-08 09:48:42 -0400

    changed search box into a component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component'
 
 import './App.css';
 
@@ -29,7 +29,11 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input type="search" placeholder="search monsters" onChange={e => this.setState({searchField: e.target.value})}/>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={e => this.setState({searchField: e.target.value})}
+        />
+
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
